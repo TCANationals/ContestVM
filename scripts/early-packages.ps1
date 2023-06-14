3fd7c33a9823da33f3a196303ea80acab14697d6
Mike Christopher <mchristopher@users.noreply.github.com>
2023-06-14 03:57:17 -0400

    try catch wsl

--- scripts/early-packages.ps1
+++ scripts/early-packages.ps1
@@ -1,7 +1,11 @@
 . C:\Packer\Scripts\tca-env.ps1
 
 # Install WSL 2
-Choco-Install -PackageName wsl2 -ArgumentList "--params", '"/Version:2 /Retry:true"'
+try {
+    Choco-Install -PackageName wsl2 -RetryCount 3 -ArgumentList "--ignore-package-exit-codes", "--params", '"/Version:2 /Retry:true"'
+} catch {
+    Write-Host "WSL install failed, ignoring..."
+}
 
 # Install the base system apps (before Horizon)
 Choco-Install -PackageName vmware-workstation-player -ArgumentList "--override=1", "--installargs=`"'/s /v/qn EULAS_AGREED=1 AUTOSOFTWAREUPDATE=0 DATACOLLECTION=0 ADDLOCAL=ALL REMOVE=Keyboard REBOOT=ReallySuppress'`""
