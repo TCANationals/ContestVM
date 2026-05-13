71d64d80c50f07478305534887569e857ce99941
Mike Christopher <mchristopher@users.noreply.github.com>
2026-05-13 14:56:54 -0700

    feat: dw notes

--- server_scripts/Populate Date Dimension.sql
+++ server_scripts/Populate Date Dimension.sql
@@ -1,8 +1,13 @@
+-- Visual Studio 2026 community
+-- from inside VS Extensions, install:
+--    SQL Server Integration Services Projects
+--      https://marketplace.visualstudio.com/items?itemName=SSIS.MicrosoftDataToolsIntegrationServices
+--    Analysis Services
 -- Run on WideWorldImportersDW
 
 DECLARE @YearCounter int = 2013;
 
-WHILE @YearCounter <= 2024
+WHILE @YearCounter <= 2026 -- set current year
 BEGIN
     EXEC Integration.PopulateDateDimensionForYear @YearCounter;
     SET @YearCounter += 1;
